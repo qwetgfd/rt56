62dff9cc37ea5fd8b78b9155e0d8ea917d68585e
sdf <hgfgf@hg.com>
2026-05-22 01:26:20 -0700

    sdfs

--- fr/src/app/sharepoint/sharepoint.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { SharepointComponent } from './sharepoint.component';
-
-/** Lazy-load or import into the host router, e.g. `{ path: 'sharepoint', children: SHAREPOINT_ROUTES }`. */
-export const SHAREPOINT_ROUTES: Routes = [
-  { path: '', component: SharepointComponent },
-];
--- fr/src/environments/environment.ts
+++
@@ -1,1 +0,0 @@
-export { DEFAULT_SHAREPOINT_ENVIRONMENT as environment } from '../app/sharepoint/core/sharepoint.config';
